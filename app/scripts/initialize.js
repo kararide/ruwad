d42c5344fe2e70f16bc7305cb4b9fbe190de6253
kararide <kararide@gmail.com>
2017-03-02 10:57:32 +0100

    added first version of sticky header -need changes, and changed order of elements

--- app/scripts/initialize.js
+++ app/scripts/initialize.js
@@ -3,10 +3,18 @@ var typed = require('typed.js');
 
 $(document).ready(function() {
     $("#typed-text").typed({
-        strings: ["założyć firmę w Dubaju?", "znaleźć kontrahenta zainteresowanego Twoimi produktami?"],
+        strings:  ["założyć firmę w Dubaju?","znaleźć kontrahenta?","prowadzić tam biznes?"],
         typeSpeed: 50,
         startDelay: 10,
         loop: true
     });
 });
 
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1){  
+    $('header').addClass("sticky");
+  }
+  else{
+    $('header').removeClass("sticky");
+  }
+});
