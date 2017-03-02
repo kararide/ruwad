cc2461d0f2073e7ac5a12dca0217521eebb320e5
kararide <kararide@gmail.com>
2017-03-02 23:41:56 +0100

    conception changed, small fixes

--- app/scripts/initialize.js
+++ app/scripts/initialize.js
@@ -9,14 +9,3 @@ $(document).ready(function() {
         loop: true
     });
 });
-
-$(function(){
-  $(window).scroll(function(){
-    var winTop = $(window).scrollTop();
-    if(winTop >= 30){
-      $("body").addClass("sticky-header");
-    }else{
-      $("body").removeClass("sticky-header");
-    }
-  });
-});
